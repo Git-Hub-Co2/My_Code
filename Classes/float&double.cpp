7cf4e67bbb887e0bfbebcd48d3fe949459255ddd
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-03 18:04:29 +0200

    Update float&double.cpp

--- Classes/float&double.cpp
+++ Classes/float&double.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 int main()
 {
-    float numFloat = 10/ 3.0f;
+    //单浮点与双浮点精度对比
+    float numFloat = 10/ 3.0f;//浮点数后加f
     double numDouble = 10 / 3.0f;
-    cout << fixed << setprecision(2);
-    cout << "numFloart = " << numFloat * 100000000 << endl;
+    cout << fixed << setprecision(2); //fixed 设定输出结果永远为浮点数
+    //setprecision(2)设定浮点数小数点后显示几位，此处为显示小数点后2位
+    cout << "numFloart = " << numFloat * 100000000 << endl;//计算
     cout << "numDouble = " << numDouble * 100000000 << endl;
     cin.get();
 
