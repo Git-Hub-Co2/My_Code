f7acb58a9a405321c9e7cf682ecded1deef02cc1
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-03 18:04:41 +0200

    Create SizeOf_App.cpp

---
+++ Classes/SizeOf_App.cpp
@@ -0,0 +1,13 @@
+/*#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int mian()
+{
+    cout << "What kind of Date wodle u want to know?" << '\n';
+    
+    cout << "This is " << sizeof(cin.get()) << "Bit." << endl;
+    
+    return 0 ;
+}
