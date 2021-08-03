850e64c9e4be80c8374f9ab463dd3cc293662bd2
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-03 16:47:44 +0200

    Create float&double.cpp

---
+++ Classes/float&double.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float numFloat = 10/ 3.0f;
+    double numDouble = 10 / 3.0f;
+    cout << fixed << setprecision(2);
+    cout << "numFloart = " << numFloat * 100000000 << endl;
+    cout << "numDouble = " << numDouble * 100000000 << endl;
+    cin.get();
+
+}
