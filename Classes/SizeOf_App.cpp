7edda7b716dce7d66214c5d045129a51df9a5221
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-03 18:11:10 +0200

    Update SizeOf_App.cpp

--- Classes/SizeOf_App.cpp
+++ Classes/SizeOf_App.cpp
@@ -1,13 +1,16 @@
-/*#include <iostream>
+//这个程序并不能执行！！！！！！！03/08/2021
+#include <iostream>
 #include <iomanip>
 
 using namespace std;
 
 int mian()
 {
+    char word = '\0';
     cout << "What kind of Date wodle u want to know?" << '\n';
-    
-    cout << "This is " << sizeof(cin.get()) << "Bit." << endl;
+    //将用户输入的文字赋新值给word
+    cin >> word;
+    cout << "This is " << sizeof(word) << "Bit." << endl;
     
     return 0 ;
 }
