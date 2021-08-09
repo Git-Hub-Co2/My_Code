1ca17c10b7772ab227ed2df2fe0f46a4e1ae34f2
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-09 23:17:31 +0200

    Update Copy.cpp

--- Classes/Copy.cpp
+++ Classes/Copy.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
 
-int MaxWert = Wert_Grundattribute[0];
+int main(){
+    int Wert_Grundattribute [1];
+
+    int MaxWert = Wert_Grundattribute[0];
         int MaxWert_Index = 0;
         for (int i = 1 ; i <5 ; i++){
         if (MaxWert < Wert_Grundattribute[i]){
             MaxWert = Wert_Grundattribute[i];
             MaxWert_Index = i;
-            }
-        
+        }
+    }
+    cin.get() ;
+}
