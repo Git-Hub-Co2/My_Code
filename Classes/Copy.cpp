58f286c9d7e9bc1cbd3bdd2d76fd5d9f3a440ff8
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-09 23:07:38 +0200

    Create Copy.cpp

---
+++ Classes/Copy.cpp
@@ -0,0 +1,10 @@
+
+
+int MaxWert = Wert_Grundattribute[0];
+        int MaxWert_Index = 0;
+        for (int i = 1 ; i <5 ; i++){
+        if (MaxWert < Wert_Grundattribute[i]){
+            MaxWert = Wert_Grundattribute[i];
+            MaxWert_Index = i;
+            }
+        
