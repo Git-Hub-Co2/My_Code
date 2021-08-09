e2d9a0e63b65c5d9602e1662a1c1b441396c943f
Mizuki_Potato <76223398+Git-Hub-Co2@users.noreply.github.com>
2021-08-09 23:07:33 +0200

    Update helloworld.cpp

--- Classes/helloworld.cpp
+++ Classes/helloworld.cpp
@@ -2,7 +2,17 @@
 * me: Yiming Dong AKA MAjor
 * Date: 01-08-2021
 * 
-*
+*Hiermit erkläre ich, dass ich die vorliegende Arbeit selbstständig und nur unter Verwendung der angegebenen Literatur und Hilfsmittel angefertigt habe.
+
+Stellen, die wörtlich oder sinngemäß aus Quellen entnommen wurden, sind als solche kenntlich gemacht.
+
+Diese Arbeit wurde in gleicher oder ähnlicher Form noch keiner anderen Prüfungsbehörde vorgelegt.
+
+ort, den tag.monat.jahr
+
+unterschrift vorname name
+
+Zuletzt geändert: Donnerstag, 15. Juli 2021, 16:20
 *
 */
 #include<iostream>//hade
